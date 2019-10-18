ee6352674af7bab5f269ecc611ac0a9b27063c65
GianlucaDiBella <gianluca.dib99@gmail.com>
2019-10-18 18:12:05 -0400

    Mis viajes, verificacion de reserva, en proceso.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FilteredSliderComponent } from './filtered-slider/filtered-slider.component';
 import { CarouselModule } from 'ngx-bootstrap/carousel';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
-
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -15,13 +15,17 @@ import { NavbarComponent } from './navbar/navbar.component';
     FilteredSliderComponent,
     HomeComponent,
     NavbarComponent,
+    routingComponents,
+    FormsModule
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     CarouselModule.forRoot(),
+    ButtonsModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule { }
+
