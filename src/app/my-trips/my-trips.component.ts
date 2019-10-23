e0ab2e93c756741ba026eb49df222732f7c10f9a
GianlucaDiBella <gianluca.dib99@gmail.com>
2019-10-23 14:22:43 -0400

    Codigo de reserva funcional

--- src/app/my-trips/my-trips.component.ts
+++ src/app/my-trips/my-trips.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-my-trips',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyTripsComponent implements OnInit {
 
-  constructor() { }
+check: boolean;
+
+  constructor(private router: Router) {
+    this.check = false;
+  }
 
   ngOnInit() {
+  }
+
+  verificationCode(event) {
+    event.preventDefault();
+    const target = event.target;
+    const reservationCheck = target.querySelector('#reservationcode').value;
+    console.log(reservationCheck);
+    if ( reservationCheck === 'DEMO') {
+      this.check = false;
+  } else {
+    this.check = true;
+  }
   }
 
 
