cb8aa1eb81787368135484cf933ed8740d727a73
Samuel Garcia <samuelalejandro0102@gmail.com>
2019-11-29 05:28:35 -0400

    ja

--- src/app/my-trips/my-trips.component.ts
+++ src/app/my-trips/my-trips.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import * as jsPDF from 'jspdf';
+import { Orden } from 'src/models/orden';
 import { Itinerario } from 'src/models/orden';
+import { ServicioService } from '../servicio.service';
 @Component({
   selector: 'app-my-trips',
   templateUrl: './my-trips.component.html',
@@ -16,24 +18,27 @@ downloadPDF() {
   doc.text('RESERVACION DE ITINERARIO', 10, 10);
   doc.save('Itinerario.pdf');
 }
-  constructor(private router: Router) {
+  constructor(private router: Router, private s: ServicioService) {
     this.check = false;
   }
 
+  orden: Orden[];
+  ordenes: Orden[];
+
   ngOnInit() {
+    this.s.getOrden().subscribe(items=>{
+      this.orden = items;
+    })
   }
 
+  ok(){}
+
   verificationCode(event) {
-    event.preventDefault();
     const target = event.target;
     const reservationCheck = target.querySelector('#reservationcode').value;
     console.log(reservationCheck);
 
-    if ( reservationCheck === 'DEMO') {
-      this.downloadPDF();
-  } else {
-    this.check = true;
-  }
+    this.ordenes = this.orden.filter(item => item.telefono === reservationCheck);
   }
 
 
